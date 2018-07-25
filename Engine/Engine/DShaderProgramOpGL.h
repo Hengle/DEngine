c371a51586de6e52c29fe11f13a53f8e3fe1f19d
lusen <huancr@outlook.com>
2018-07-25 13:28:45 +0800

    glm

--- Engine/Engine/DShaderProgramOpGL.h
+++ Engine/Engine/DShaderProgramOpGL.h
@@ -8,6 +8,34 @@ class DShaderProgramOpGL : public DShaderProgram
 public:
 	DShaderProgramOpGL();
 	~DShaderProgramOpGL();
+	virtual void GetResDesc(unsigned int index, DShaderResDesc&) const;
+	virtual bool HasProperty(const LPCSTR key) const;
+	virtual void Release();
+
+protected:
+	virtual void OnDraw();
+	virtual void OnApplyParams(std::map<std::string, float*>&params, std::map<std::string, float*>&gparams);
+};
+
+class DShaderVertexProgramOpGL : public DShaderProgramOpGL
+{
+public:
+	DShaderVertexProgramOpGL();
+	~DShaderVertexProgramOpGL();
+	virtual void Release();
+
+protected:
+
+};
+
+class DShaderPixelProgramOpGL : public DShaderProgramOpGL
+{
+public:
+	DShaderPixelProgramOpGL();
+	~DShaderPixelProgramOpGL();
+
+protected:
+
 };
 
 #endif
